c4b1ab6db836dc8aad93674841cc2a38e549d21d
JesusRA <ramosxdjesus@gmail.com>
2026-03-16 15:15:42 -0600

    Feat: Seguridad en las rutas del backend

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { Public } from '../../common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public() // <--- Esta es la "llave" que salta el Guard Global
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
--- backend/src/modules/maintenance/vehiculos/vehiculos.controller.ts
+++ backend/src/modules/maintenance/vehiculos/vehiculos.controller.ts
@@ -1,19 +1,22 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { VehiculosService } from './vehiculos.service';
 import { CreateVehiculoDto } from './dto/create-vehiculo.dto';
-// import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard'; // Descomenta si ya quieres usar seguridad
+import { Roles } from '../../../common/decorators/roles.decorator'; // Ajusta la ruta
+import { Public } from '../../../common/decorators/public.decorator'; // Ajusta la ruta
 
 @Controller('vehiculos')
-// @UseGuards(JwtAuthGuard) // Si quieres proteger el CRUD desde ya
 export class VehiculosController {
   constructor(private readonly vehiculosService: VehiculosService) {}
 
   @Post()
+  @Roles('superAdmin', 'logístico') // Solo estos roles pueden crear vehículos
   create(@Body() createVehiculoDto: CreateVehiculoDto) {
     return this.vehiculosService.create(createVehiculoDto);
   }
 
   @Get()
+  // No necesita @UseGuards porque es GLOBAL. 
+  // Requiere token por defecto.
   findAll() {
     return this.vehiculosService.findAll();
   }
@@ -24,12 +27,14 @@ export class VehiculosController {
   }
 
   @Patch(':id')
-update(@Param('id') id: string, @Body() updateDto: Partial<CreateVehiculoDto>) {
-  return this.vehiculosService.update(id, updateDto);
-}
+  @Roles('superAdmin', 'logístico') // Solo el admin puede editar
+  update(@Param('id') id: string, @Body() updateDto: Partial<CreateVehiculoDto>) {
+    return this.vehiculosService.update(id, updateDto);
+  }
 
   @Delete(':id')
+  @Roles('admin') // Solo el admin puede eliminar
   remove(@Param('id') id: string) {
-  return this.vehiculosService.remove(id);
-}
+    return this.vehiculosService.remove(id);
+  }
 }
