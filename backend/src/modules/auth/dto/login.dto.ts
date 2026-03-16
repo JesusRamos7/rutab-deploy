9a18045dc92694786a3e276a3b00ca9a08d8a854
JesusRA <ramosxdjesus@gmail.com>
2026-03-16 14:15:03 -0600

    fix: Incompatibilidades post merge

--- backend/src/modules/auth/dto/login.dto.ts
+++ backend/src/modules/auth/dto/login.dto.ts
@@ -1,6 +1,17 @@
-// backend/src/auth/dto/login.dto.ts
+import { IsEmail, IsNotEmpty, IsString, IsEnum } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'El formato del correo es inválido' })
+  @IsNotEmpty({ message: 'El correo es obligatorio' })
   correo: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
   password: string;
-  tipoAcceso: 'ADMIN' | 'CHOFER'; // <- Añadimos el tipo de acceso esperado
+
+  @IsEnum(['ADMIN', 'CHOFER'], { 
+    message: 'El tipo de acceso debe ser ADMIN o CHOFER' 
+  })
+  @IsNotEmpty()
+  tipoAcceso: 'ADMIN' | 'CHOFER';
 }
