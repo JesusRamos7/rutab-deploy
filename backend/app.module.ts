7d21dbe6cd2679cf20030588152e3b10fb3c3c0c
JesusRA <ramosxdjesus@gmail.com>
2026-04-02 16:09:13 -0600

    refactor: Consistencia de rutas de archivos

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -7,10 +7,10 @@ import { AuthModule } from './src/modules/auth/auth.module';
 import { VehiclesModule } from './src/modules/vehicles/vehicles.module';
 import { JwtAuthGuard } from './src/common/guards/jwt-auth.guard';
 import { RolesGuard } from './src/common/guards/roles.guard';
-import { CustomersModule } from 'src/modules/customers/customers.module';
-import { DriversModule } from 'src/modules/drivers/drivers.module';
-import { OptimizacionModule } from 'src/modules/optimizacion/optimizacion.module';
-import { OrdersModule } from 'src/modules/orders/orders.module';
+import { CustomersModule } from './src/modules/customers/customers.module';
+import { DriversModule } from './src/modules/drivers/drivers.module';
+import { OptimizacionModule } from './src/modules/optimizacion/optimizacion.module';
+import { OrdersModule } from './src/modules/orders/orders.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, VehiclesModule, CustomersModule, DriversModule, OptimizacionModule, OrdersModule],
