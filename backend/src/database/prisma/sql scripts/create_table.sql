e8b87159b70994bb6deb0990352e45f55586e91a
JesusRA <ramosxdjesus@gmail.com>
2026-04-20 22:25:37 -0600

    fix: ; en vez de ,

--- backend/src/database/prisma/sql scripts/create_table.sql
+++ backend/src/database/prisma/sql scripts/create_table.sql
@@ -162,7 +162,7 @@ CREATE TABLE public.incidencias (
   tipo text,
   descripcion text,
   foto_url text,
-  coordenadas_incidente GEOGRAPHY(Point, 4326);
+  coordenadas_incidente GEOGRAPHY(Point, 4326),
   created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT incidencias_pkey PRIMARY KEY (id),
